11b7a5ac50fd7c5014b284d624dc7b9a70a3439a
Andrey Komendantov <komandr@gmail.com>
2022-10-31 05:03:05 +0300

    feat: updating avalanche_utils.py

--- src/service/avalanche_utils.py
+++ src/service/avalanche_utils.py
@@ -1,3 +1,5 @@
+import json
+import requests
 from web3 import Web3
 from web3.middleware import geth_poa_middleware
 from src.service.settings import config
@@ -20,6 +22,63 @@ def get_blockchain_w3(blockchain):
     w3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
     return w3
+
+
+def get_contract_events(from_block: int, to_block: int):
+    api_key_token = "ApiKeyToken"
+    url_host = "https://api.snowtrace.io/api?module=logs&action=getLogs"
+    url_request = f"&fromBlock={from_block}&toBlock={to_block}&address=0x66357dCaCe80431aee0A7507e2E361B7e2402370"
+    api_key = f"&apikey={api_key_token}"
+    result_url = url_host + url_request + api_key
+
+    r = requests.get(result_url)
+    print(json.loads(r.content)['result'])
+
+    return
+    web3 = get_blockchain_w3(BlockchainName.Avalanche)
+    with open('abi_avax_token.json') as json_file:
+        abi_data = json.load(json_file)
+    avalanche_contract = web3.eth.contract(address=config.AVALANCHE_SMART_CONTRACT_ADDRESS,
+                                           abi=abi_data)
+    print(avalanche_contract.address)
+
+    # events = avalanche_contract.pastEvents('Transfer').call()
+
+    # avalanche_contract.methods.getC1().call()
+
+    from web3._utils.events import get_event_data
+    from web3._utils.filters import construct_event_filter_params
+
+    # events = avalanche_contract.events\
+
+    # events.eventFilter('Transfer', {'fromBlock': 0, 'toBlock': 'latest'})
+    # print(events)
+    # for even in events:
+    #     print(1)
+    #     print(even.event_name)
+
+    # event_filter = web3.eth.filter({"address": avalanche_contract})
+
+    event_filter = avalanche_contract.events.create_filter(fromBlock="latest", argument_filters={'arg1': 10})
+    event_filter.get_new_entries()
+
+    # event_filter = web3.eth.filter({
+    #     # "fromBlock": 0,
+    #     # "toBlock": "latest",
+    #     "address": avalanche_contract,
+    # })
+
+    print(event_filter)
+
+
+    # res_event = events.eventFilter('Transfer', {'fromBlock': 0, 'toBlock': 'latest'})
+
+    # events = avalanche_contract.eventFilter('Transfer', {'fromBlock': 0, 'toBlock': 'latest'})
+
+    # print('events', events)
+    # ('MyEvent', fromBlock: 0, toBlock: 'latest').call()
+
+    return avalanche_contract
 
 
 def get_avalanche_balance(address):
@@ -45,4 +104,5 @@ def get_blockchain_balance_block(address, block, blockchain):
     return JSONResponse({Decimal(balance) / Decimal(10**decimals)})
 
 
+get_contract_events(21753727, 21753909)
 # print(get_avalanche_balance('0x66357dCaCe80431aee0A7507e2E361B7e2402370'))
